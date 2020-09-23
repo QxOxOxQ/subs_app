57fe9b8c36feaa73cd34fe261134701b3cf962d4
Adam Ludian <adamludian@gmail.com>
2020-09-23 14:24:29 +0200

    Add product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_163709) do
+ActiveRecord::Schema.define(version: 2020_09_23_115245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_09_28_163709) do
   create_table "blacklists", force: :cascade do |t|
     t.string "jti", null: false
     t.index ["jti"], name: "index_blacklists_on_jti"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
