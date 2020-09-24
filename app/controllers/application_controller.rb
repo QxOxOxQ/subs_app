579ab063715e4782392454f2a4b0ab8d79a85637
Adam Ludian <adamludian@gmail.com>
2020-09-24 12:27:18 +0200

    handle errors from  aplication controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_unprocessable_entity_response(exception)
+    render json: exception.record.errors, status: :unprocessable_entity
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+  
   def render_resource(resource)
     if resource.errors.empty?
       render json: resource
